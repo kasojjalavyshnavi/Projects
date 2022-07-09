d3aee4348899f9e2ea40c733d0098119a0491b40
kasojjalavyshnavi <kasojjalavyshnavi89@gmail.com>
2022-07-09 16:49:16 +0530

    project

---
+++ FINALPROJECT/MiniProject/MiniProject/Models/Product.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace MiniProject.Models
+{
+    public class Product
+    {
+        [Key]
+        public int ProductID { get; set; }
+        [Required]
+        public string ProductName { get; set; }
+        [DataType(DataType.Currency)]
+        public Double? Price { get; set; }
+        public string Brand { get; set; }
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0 : yyyy-mm-dd}", ApplyFormatInEditMode = true)]
+        public string ManufactureDate { get; set; }
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-mm-dd}", ApplyFormatInEditMode = true)]
+        public DateTime ExpirationDate { get; set; }
+    }
+}
+
